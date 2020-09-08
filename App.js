d1d12f71a894ed35380c808fd63e587291cda8e2
alestor123 <alestoraldous@gmail.com>
2020-09-08 17:35:53 +0530

    added help

--- App.js
+++ App.js
@@ -2,3 +2,10 @@
 
 var { spawn } = require('child_process'),
     name = process.argv[2];
+if (!name || name.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
+        return console.log(`
+        Invalid directory name.
+        Usage: quick-create-mern name-of-app  
+      `);
+}
+      

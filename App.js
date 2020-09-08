b1b5f572a4ba98645cd8ac5131d7044bc20aec14
alestor123 <alestoraldous@gmail.com>
2020-09-08 17:39:23 +0530

    added run command

--- App.js
+++ App.js
@@ -25,5 +25,21 @@ runCommand('git', ['clone', repoURL, name])
     console.log('npm run dev');
   });
 
-
+function runCommand(command, args, options = undefined) {
+    const spawned = spawn(command, args, options);  
+    return new Promise((resolve) => {
+      spawned.stdout.on('data', (data) => {
+        console.log(data.toString());
+      });
       
+      spawned.stderr.on('data', (data) => {
+        console.error(data.toString());
+      });
+      
+      spawned.on('close', () => {
+        resolve();
+      });
+    });
+  }
+  
+    

204487ba9a8c9676a26d63dfdabede2029b91689
alestor123 <alestoraldous@gmail.com>
2020-09-08 17:37:12 +0530

    added git repo url

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 #! /usr/bin/env node
 
 var { spawn } = require('child_process'),
-    name = process.argv[2];
+    name = process.argv[2],
+repoURL = 'https://github.com/alestor123/MERN-CLI.git';
 if (!name || name.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
         return console.log(`
         Invalid directory name.
         Usage: quick-create-mern name-of-app  
       `);
 }
+
+
       
